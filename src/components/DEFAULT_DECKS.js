176f4e5ece67676423739ad642221daed8e8775e
evilpaper <pellburt@gmail.com>
2021-03-17 06:28:28 +0100

    Rephrase definitions on cards

--- src/components/DEFAULT_DECKS.js
+++ src/components/DEFAULT_DECKS.js
@@ -7,52 +7,59 @@ const DEFAULT_DECKS = [
     cards: [
       {
         id: nanoid(),
-        question: "The  _____  function evaluates JavaScript code represented as a string.",
+        question:
+          "Function that evaluates JavaScript code represented as a string.",
         answer: "eval() ",
         nailed: false,
       },
       {
         id: nanoid(),
-        question: "The global  _____  property represents the primitive value Not-A-Number.",
+        question:
+          "Global property that represents the primitive value Not-A-Number.",
         answer: "NaN",
         nailed: false,
       },
       {
         id: nanoid(),
-        question: "The global  _____  property represents the primitive value undefined.",
+        question:
+          "Global property that represents the primitive value undefined.",
         answer: "undefined",
         nailed: false,
       },
       {
         id: nanoid(),
-        question: "The  _____  object are thrown when runtime errors occur.",
+        question: "Object that are thrown when runtime errors occur.",
         answer: "Error",
         nailed: false,
       },
       {
         id: nanoid(),
-        question: "The  _____  object holds key-value pairs and remembers the original insertion order of the keys.  ",
+        question:
+          "Object that holds key-value pairs and remembers the original insertion order of the keys.",
         answer: "Map",
         nailed: false,
       },
       {
         id: nanoid(),
-        question: "The  _____  object lets you store unique values of any type, whether primitive values or object references.",
+        question:
+          "Object that lets you store unique values of any type, whether primitive values or object references.",
         answer: "Set",
         nailed: false,
       },
       {
         id: nanoid(),
-        question: "The  _____  object contains methods for parsing JavaScript Object Notation (JSON) and converting values to JSON.",
+        question:
+          "Object that contains methods for parsing JavaScript Object Notation (JSON) and converting values to JSON.",
         answer: "JSON",
         nailed: false,
       },
       {
         id: nanoid(),
-        question: "The  _____  object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.",
+        question:
+          "Object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value.",
         answer: "Promise",
         nailed: false,
-      }
+      },
     ],
   },
 ];
