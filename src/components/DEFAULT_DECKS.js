11d705b8c5ce7b771f09a0347464aed94935559d
evilpaper <pellburt@gmail.com>
2021-03-21 21:13:31 +0100

    Add new cards

--- src/components/DEFAULT_DECKS.js
+++ src/components/DEFAULT_DECKS.js
@@ -8,13 +8,6 @@ const DEFAULT_DECKS = [
       {
         id: nanoid(),
         question:
-          "Function that evaluates JavaScript code represented as a string.",
-        answer: "eval() ",
-        nailed: false,
-      },
-      {
-        id: nanoid(),
-        question:
           "Global property that represents the primitive value Not-A-Number.",
         answer: "NaN",
         nailed: false,
@@ -58,6 +51,34 @@ const DEFAULT_DECKS = [
         question:
           "Object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value.",
         answer: "Promise",
+        nailed: false,
+      },
+      {
+        id: nanoid(),
+        question:
+          "Method takes an iterable of promises as an input, and returns a single Promise that resolves to an array of the results of the input promises",
+        answer: "Promise.all()",
+        nailed: false,
+      },
+      {
+        id: nanoid(),
+        question:
+          "Array-like object accessible inside functions that contains the values of the arguments passed to that function.",
+        answer: "arguments",
+        nailed: false,
+      },
+      {
+        id: nanoid(),
+        question:
+          "Syntax allows us to represent an indefinite number of arguments as an array.",
+        answer: "The rest parameter",
+        nailed: false,
+      },
+      {
+        id: nanoid(),
+        question:
+          "Allows an iterable such as an array expression or string to be expanded in places where zero or more arguments (for function calls)",
+        answer: "Spread syntax (...)",
         nailed: false,
       },
     ],
